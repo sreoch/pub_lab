3ad95e37cddf5ef07f37a15d132b3356b669ef85
NaomiValuks <naomi@valuks.com>
2021-11-17 11:53:52 +0000

    started the integration

--- src/pub.py
+++ src/pub.py
@@ -2,9 +2,18 @@ class Pub:
     def __init__(self, name, till):
         self.name = name
         self.till = till
-        self.drinks =[]
-        
-    
+        self.drinks = []
 
+    
     def increase_till(self, amount):
         self.till += amount
+    
+    def add_drink(self, drink):
+        self.drinks.append(drink)
+    
+    def stock_count(self):
+        return len(self.drinks)
+
+    def sell_drink_to_customer(self, amount, customer):
+        customer.wallet_decrease(amount)
+        self.increase_till(amount)
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -1,10 +1,14 @@
 import unittest
 from src.pub import Pub
+from src.drink import Drink
+from src.customer import Customer
 
 class TestPub(unittest.TestCase):
     
     def setUp(self):
         self.pub = Pub("The Prancing Pony", 100.00)
+        self.drink = Drink("Bloody Mary", 5.50)
+        self.customer = Customer("Noah Clark", 35.00)
 
     
     def test_pub_has_name(self):
@@ -16,3 +20,15 @@ class TestPub(unittest.TestCase):
     def test_increase_till(self):
         self.pub.increase_till(2.50)
         self.assertEqual(102.50, self.pub.till)
+
+    def test_drinks_list_length(self):
+        self.pub.add_drink(self.drink)
+        self.pub.stock_count()
+        self.assertEqual(1, self.pub.stock_count())
+
+    def test_sell_drink_to_customer(self):
+        self.pub.increase_till(5.50)
+        self.customer.wallet_decrease(5.50)
+        
+        self.assertEqual(105.50, self.pub.till)
+        self.assertEqual(29.50, self.customer.wallet)
